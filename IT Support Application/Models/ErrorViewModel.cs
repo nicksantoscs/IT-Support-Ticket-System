870c570eeb0d541e093ed065e223b0a66143b887
Nicholas Santos <santos.nick27@gmail.com>
2024-01-17 15:25:01 -0500

    Initial commit

---
+++ IT Support Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace IT_Support_Application.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
